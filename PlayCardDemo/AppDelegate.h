a15cc23236e35d7148bab4813c7810e9e6367b30
QSKOBE24 <398706336@qq.com>
2017-03-17 20:03:44 +0800

    Initial Commit

---
+++ PlayCardDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlayCardDemo
+//
+//  Created by qskobe24 on 2017/3/17.
+//  Copyright © 2017年 qskobe24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlayCardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayCardDemo
+//
+//  Created by qskobe24 on 2017/3/17.
+//  Copyright © 2017年 qskobe24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
